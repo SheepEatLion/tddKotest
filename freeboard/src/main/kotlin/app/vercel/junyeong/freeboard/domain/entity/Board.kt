d83edc6c51dab5167a0f70964402c97a7b5b456c
SheepEatLion <gowjr207@gmail.com>
2023-10-18 18:38:59 +0900

    feat: create board domain

--- freeboard/src/main/kotlin/app/vercel/junyeong/freeboard/domain/entity/Board.kt
+++ freeboard/src/main/kotlin/app/vercel/junyeong/freeboard/domain/entity/Board.kt
@@ -1,4 +1,22 @@
 package app.vercel.junyeong.freeboard.domain.entity
 
-class Board {
-}
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType.AUTO
+import jakarta.persistence.Id
+import java.time.LocalDateTime
+
+@Entity
+class Board(
+    @Id
+    @GeneratedValue(strategy = AUTO)
+    val id: Long,
+
+    val title: String,
+
+    val contents: String,
+
+    val createdAt: LocalDateTime,
+
+    var updatedAt: LocalDateTime,
+)
