712c443d7ca114485da192835a26b96310eabd37
SheepEatLion <gowjr207@gmail.com>
2023-10-21 23:31:06 +0900

    build: kotest 의존성 호환 버전 적용

--- freeboard/build.gradle.kts
+++ freeboard/build.gradle.kts
@@ -19,17 +19,6 @@ repositories {
 	mavenCentral()
 }
 
-dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	developmentOnly("org.springframework.boot:spring-boot-devtools")
-	runtimeOnly("com.h2database:h2")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	testImplementation("io.kotest:kotest-runner-junit5:5.7.0")
-}
-
 tasks.withType<KotlinCompile> {
 	kotlinOptions {
 		freeCompilerArgs += "-Xjsr305=strict"
@@ -39,4 +28,23 @@ tasks.withType<KotlinCompile> {
 
 tasks.withType<Test>().configureEach {
 	useJUnitPlatform()
+}
+
+val kotestVersion = "5.6.1"
+val kotestExtensionsVersion = "1.1.3"
+val mockkVersion = "1.13.8"
+val mockServerVersion = "5.11.1"
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	developmentOnly("org.springframework.boot:spring-boot-devtools")
+	runtimeOnly("com.h2database:h2")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("io.kotest:kotest-runner-junit5-jvm:$kotestVersion")
+	testImplementation("io.kotest:kotest-assertions-core-jvm:$kotestVersion")
+	testImplementation("io.kotest:kotest-property-jvm:$kotestVersion")
+	testImplementation("io.kotest.extensions:kotest-extensions-spring:$kotestExtensionsVersion")
 }
