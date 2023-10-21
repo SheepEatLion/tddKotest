964167b2a5e8248f76c5f613f15769b56207305c
SheepEatLion <gowjr207@gmail.com>
2023-10-21 18:50:55 +0900

    feat: 단일 조회 기능 구현

--- freeboard/src/main/kotlin/app/vercel/junyeong/freeboard/application/BoardService.kt
+++ freeboard/src/main/kotlin/app/vercel/junyeong/freeboard/application/BoardService.kt
@@ -1,4 +1,15 @@
 package app.vercel.junyeong.freeboard.application
 
-class BoardService {
+import app.vercel.junyeong.freeboard.domain.entity.Post
+import app.vercel.junyeong.freeboard.domain.repository.PostRepository
+import app.vercel.junyeong.freeboard.exception.NotFoundException
+import org.springframework.stereotype.Service
+
+@Service
+class BoardService(
+    private val postRepository: PostRepository,
+) {
+    fun getPost(id: Long): Post {
+        return postRepository.findById(id).orElseThrow { throw NotFoundException() }
+    }
 }
