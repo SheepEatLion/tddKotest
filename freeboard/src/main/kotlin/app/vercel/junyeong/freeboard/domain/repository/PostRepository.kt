ac07f27a5c07f2e679039ca8ecd2312478cbd8a2
SheepEatLion <gowjr207@gmail.com>
2023-10-21 18:33:48 +0900

    feat: POST jpa repository

--- freeboard/src/main/kotlin/app/vercel/junyeong/freeboard/domain/repository/PostRepository.kt
+++ freeboard/src/main/kotlin/app/vercel/junyeong/freeboard/domain/repository/PostRepository.kt
@@ -1,4 +1,9 @@
 package app.vercel.junyeong.freeboard.domain.repository
 
-class PostRepository {
+import app.vercel.junyeong.freeboard.domain.entity.Post
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface PostRepository : JpaRepository<Post, Long> {
 }
