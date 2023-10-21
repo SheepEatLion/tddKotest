9dbbbecace293b3983ddcc69507c50fe35ac781c
SheepEatLion <gowjr207@gmail.com>
2023-10-21 23:01:09 +0900

    test: 제목에 re: 자동생성 기능 테스트 성공

--- freeboard/src/test/kotlin/app/vercel/junyeong/freeboard/tdd/TitleAutoUpdateFeatureTest.kt
+++ freeboard/src/test/kotlin/app/vercel/junyeong/freeboard/tdd/TitleAutoUpdateFeatureTest.kt
@@ -1,19 +1,30 @@
 package app.vercel.junyeong.freeboard.tdd
 
 import app.vercel.junyeong.freeboard.application.BoardService
+import app.vercel.junyeong.freeboard.domain.entity.Post
 import app.vercel.junyeong.freeboard.domain.repository.PostRepository
+import app.vercel.junyeong.freeboard.presentation.data.UpdatePostRequest
 import io.kotest.core.spec.style.ExpectSpec
+import io.kotest.matchers.shouldBe
 
 class TitleAutoUpdateFeatureTest(
     private val postRepository: PostRepository
 ) : ExpectSpec() {
-    private val boardService = BoardService(repository = postRepository)
+    private val boardService = BoardService(postRepository = postRepository)
 
     init {
         context("글이 수정될 때") {
-            boardService.update()
+            val post = postRepository.save(
+                Post(
+                    title = "test",
+                    contents = "normal-contents",
+                )
+            )
+            val updatePostRequest = UpdatePostRequest(post.id, post.title, "updated contents")
+
+            boardService.update(updatePostRequest = updatePostRequest)
             expect("제목 앞에 re: 글자가 자동 생성 된다.") {
-                val result = boardService.getPost().title
+                val result = boardService.getPost(post.id).title
 
                 result.take(3) shouldBe "re:"
             }
