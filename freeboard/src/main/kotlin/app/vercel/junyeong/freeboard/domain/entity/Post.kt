eee01e85f6bee32fdb61d45b1996163dc5caed99
SheepEatLion <gowjr207@gmail.com>
2023-10-21 18:29:05 +0900

    fix: 포스트 도메인 어노테이션 수정

--- freeboard/src/main/kotlin/app/vercel/junyeong/freeboard/domain/entity/Post.kt
+++ freeboard/src/main/kotlin/app/vercel/junyeong/freeboard/domain/entity/Post.kt
@@ -1,9 +1,12 @@
 package app.vercel.junyeong.freeboard.domain.entity
 
+import jakarta.persistence.Column
 import jakarta.persistence.Entity
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType.AUTO
 import jakarta.persistence.Id
+import org.springframework.data.annotation.CreatedDate
+import org.springframework.data.annotation.LastModifiedDate
 import java.time.LocalDateTime
 
 @Entity
@@ -12,11 +15,14 @@ class Post(
     @GeneratedValue(strategy = AUTO)
     val id: Long,
 
+    @Column(length = 15)
     val title: String,
 
     val contents: String,
 
+    @CreatedDate
     val createdAt: LocalDateTime,
 
+    @LastModifiedDate
     var updatedAt: LocalDateTime,
 )
