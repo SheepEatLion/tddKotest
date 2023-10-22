c706fd10408315f82a9b5353eb9ebd4878c51a27
SheepEatLion <gowjr207@gmail.com>
2023-10-22 21:42:58 +0900

    build: mockk 의존성 추가

--- freeboard/build.gradle.kts
+++ freeboard/build.gradle.kts
@@ -38,7 +38,7 @@ allOpen {
 
 val kotestVersion = "5.6.1"
 val kotestExtensionsVersion = "1.1.3"
-val mockkVersion = "1.13.8"
+val mockkVersion = "3.0.1"
 val mockServerVersion = "5.11.1"
 val h2Version = "2.0.202"
 
@@ -54,4 +54,5 @@ dependencies {
 	testImplementation("io.kotest:kotest-assertions-core-jvm:$kotestVersion")
 	testImplementation("io.kotest:kotest-property-jvm:$kotestVersion")
 	testImplementation("io.kotest.extensions:kotest-extensions-spring:$kotestExtensionsVersion")
+	testImplementation("com.ninja-squad:springmockk:$mockkVersion")
 }
